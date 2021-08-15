0f05cdcbbc99a8785f0c5526032f01d48e9b9cea
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2021-08-15 10:29:11 +0700

    Add: admin starter layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,9 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', [DashboardController::class , 'index'])->name('dashboard');
+});
 
 Route::prefix('/student')->group(function() {
     require __DIR__.'/student.php';
